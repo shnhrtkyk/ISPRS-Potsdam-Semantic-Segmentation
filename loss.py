01dd5e9764476581b42f71b5723826bb77434559
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2019-05-29 16:46:08 +0900

    Update loss.py

--- loss.py
+++ loss.py
@@ -179,3 +179,20 @@ def dice_loss(y_true, y_pred):
   denominator = tf.reduce_sum(y_true + tf.square(y_pred))
 
   return numerator / (denominator + tf.keras.backend.epsilon())
+
+def focal_loss(alpha=0.25, gamma=2):
+  def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
+    weight_a = alpha * (1 - y_pred) ** gamma * targets
+    weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
+    
+    return (tf.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b 
+
+  def loss(y_true, y_pred):
+    y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+    logits = tf.log(y_pred / (1 - y_pred))
+
+    loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
+
+    return tf.reduce_mean(loss)
+
+  return loss
